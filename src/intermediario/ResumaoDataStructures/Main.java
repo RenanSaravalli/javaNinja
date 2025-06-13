9a99cf38a6977040762a916275fe7752a65b2fb1
RenanSaravalli <renansaravalli@gmail.com>
2025-06-12 21:53:34 -0300

    Aprendendo sobre as Queues. As filas onde o primeiro elemento a entrar na Queue vai ser o primeiro elemento a sair

--- src/intermediario/ResumaoDataStructures/Main.java
+++ src/intermediario/ResumaoDataStructures/Main.java
@@ -1,8 +1,6 @@
 package intermediario.ResumaoDataStructures;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,6 +32,51 @@ public class Main {
         System.out.println("Topo Pilha: " + nomesNinjaStack.peek());
         System.out.println("Removendo o topo: " + nomesNinjaStack.pop());
         System.out.println("Pilha atualizada: " + nomesNinjaStack);
+
+        System.out.println("\nQUEUES");
+        // ATENÇÃO AGORA VAMOS APRENDER SOBRE AS QUEUES -> FILAS
+
+        Queue<String> ninjasQueue = new LinkedList<>();
+        // Adicionando ninjas para a Queue
+        ninjasQueue.add("Naruto");
+        ninjasQueue.add("Sasuke");
+        ninjasQueue.add("Sakura");
+        ninjasQueue.add("Kakashi");
+
+        // Mostrando os itens da nossa Queue
+        System.out.println("Ninjas na fila: " + ninjasQueue);
+
+        // Tirar um ninja da fila
+        ninjasQueue.poll(); // vai direto no primeiro elemento da fila
+
+        System.out.println("Ninjas na fila depois do poll: " + ninjasQueue);
+
+        // Pomos mostrar quem é o primeiro elemento da fila
+        System.out.println("Primeiro ninja na fila do lamen: " + ninjasQueue.peek());
+
+        // Adicionando na fila
+        ninjasQueue.add("Tobirama");
+        ninjasQueue.add("Rock Lee");
+
+        System.out.println("Ninjas na fila: " + ninjasQueue);
+
+        // Esvaziando a fila.
+        ninjasQueue.poll();
+        ninjasQueue.poll();
+        ninjasQueue.poll();
+        ninjasQueue.poll();
+        ninjasQueue.poll();
+
+        // Podemos verificar se a fila está vazia:
+        if (ninjasQueue.isEmpty()) {
+            System.out.println("A fila está vazia: " + ninjasQueue);
+        } else  {
+            int contador = 0;
+            for (int i = 0; i < ninjasQueue.size(); i++) {
+                contador+=1;
+            }
+            System.out.println("A fila ainda tem: " + contador + " elementos");
+        }
 
     }
 }
